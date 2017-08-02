1e9c1804632767be99a10cf89ad5397ad114fba7
Ryan Burns <ryan.burns@clever.com>
2017-08-01 18:29:43 -0700

    Fix IAM role when not local

--- run_kcl.sh
+++ run_kcl.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-set -eu
+set -e
 
-export AWS_CREDENTIAL_PROFILES_FILE=$AWS_SHARED_CREDENTIALS_FILE
+# For running locally with an AIM role
+if [ "$AWS_SHARED_CREDENTIALS_FILE" != "" ] ; then
+    export AWS_CREDENTIAL_PROFILES_FILE=$AWS_SHARED_CREDENTIALS_FILE
+fi
 command -v java >/dev/null 2>&1 || { echo >&2 "Java not installed. Install java!"; exit 1; }
 exec java -cp "jars/*"  com.amazonaws.services.kinesis.multilang.MultiLangDaemon consumer.properties
